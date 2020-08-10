4268e668cce3fe0bba529638164a022f78f9d4ae
Armin Novak <armin.novak@thincast.com>
2020-08-10 16:39:49 +0200

    Refactored rdg_read_http_unicode_string warning fixes
    (cherry picked from commit bfa07e701d25fc936c84ad36d58c6ab1a9a33ec9)

--- libfreerdp/core/gateway/rdg.c
+++ libfreerdp/core/gateway/rdg.c
@@ -308,10 +308,7 @@ static const char* capabilities_enum_to_string(UINT32 capabilities)
 
 static BOOL rdg_read_http_unicode_string(wStream* s, const WCHAR** string, UINT16* lengthInBytes)
 {
-	union {
-		BYTE* b;
-		WCHAR* w;
-	} str;
+	WCHAR* str;
 	UINT16 strLenBytes;
 	size_t rem = Stream_GetRemainingLength(s);
 
@@ -337,7 +334,7 @@ static BOOL rdg_read_http_unicode_string(wStream* s, const WCHAR** string, UINT1
 
 	/* return the string data (if wanted) */
 	if (string)
-		*string = str.w;
+		*string = str;
 	if (lengthInBytes)
 		*lengthInBytes = strLenBytes;
 
@@ -2128,7 +2125,7 @@ static BOOL rdg_process_service_message(rdpRdg* rdg, wStream* s)
 
 static BOOL rdg_process_service_message(rdpRdg* rdg, wStream* s)
 {
-	WCHAR* msg;
+	const WCHAR* msg;
 	UINT16 msgLenBytes;
 	rdpContext* context = rdg->context;
 	assert(context);
