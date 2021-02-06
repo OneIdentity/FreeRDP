ed4816ffd46bde90d063e4845e609385ed5d352b
akallabeth <akallabeth@posteo.net>
2021-02-06 12:21:03 +0100

    refactored smartcard allocations
    (cherry picked from commit 27ba319c89ab46431b0df1b9ad41daf68b7eae6f)

--- channels/smartcard/client/smartcard_operations.c
+++ channels/smartcard/client/smartcard_operations.c
@@ -244,6 +244,7 @@ static LONG smartcard_EstablishContext_Decode(SMARTCARD_DEVICE* smartcard,
 	{
 		return log_status_error(TAG, "smartcard_unpack_establish_context_call", status);
 	}
+	operation->call.establishContext = call;
 
 	return SCARD_S_SUCCESS;
 }
