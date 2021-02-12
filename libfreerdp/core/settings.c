64f672e61a97ce153acf35e6cd711c4c5c1c2e91
akallabeth <akallabeth@posteo.net>
2021-02-12 08:30:01 +0100

    Fixed formatting and source/destination length use
    (cherry picked from commit f0ba8d7fb9c21b1134a3089eda661852b2861c0e)

--- libfreerdp/core/settings.c
+++ libfreerdp/core/settings.c
@@ -731,10 +731,11 @@ static BOOL freerdp_settings_int_buffer_copy(rdpSettings* _settings, const rdpSe
 		if (!_settings->ServerCertificate)
 			goto out_fail;
 
-		CopyMemory(_settings->ServerCertificate, settings->ServerCertificate, _settings->ServerCertificateLength);
+		CopyMemory(_settings->ServerCertificate, settings->ServerCertificate,
+		           settings->ServerCertificateLength);
 		_settings->ServerCertificateLength = settings->ServerCertificateLength;
 	}
-	
+
 	if (settings->RdpServerCertificate)
 	{
 		_settings->RdpServerCertificate = certificate_clone(settings->RdpServerCertificate);
